5b052739373b9cf59719ef529256cfe93a37e81c
tgaff <tgaff@alumni.nd.edu>
2015-12-07 20:24:10 -0800

    Sprint 1 Step 3

--- models/album.js
+++ models/album.js
@@ -1,2 +1,13 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
+
+var AlbumSchema = new Schema({
+  artistName: String,
+  name: String,
+  releaseDate: String,
+  genres: [ String ]
+});
+
+var Album = mongoose.model('Album', AlbumSchema);
+
+module.exports = Album;
--- models/index.js
+++ models/index.js
@@ -1,2 +1,6 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/tunely");
+
+var Album = require('./album');
+
+module.exports.Album = Album;
