b0997b9bc34f0147a5e367b07363e331026718f2
tgaff <tgaff@alumni.nd.edu>
2015-12-07 20:36:22 -0800

    Sprint 1 Step 4: seeding complete

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/tunely");
-
+mongoose.connect("mongodb://localhost/tunely_test");
 var Album = require('./album');
 
 module.exports.Album = Album;
--- seed.js
+++ seed.js
@@ -3,13 +3,36 @@
 
 var db = require("./models");
 
-var albumsList =[
-  // put data here!
-];
+var albumList =[];
+albumList.push({
+              artistName: 'Nine Inch Nails',
+              name: 'The Downward Spiral',
+              releaseDate: '1994, March 8',
+              genres: [ 'industrial', 'industrial metal' ]
+            });
+albumList.push({
+              artistName: 'Metallica',
+              name: 'Metallica',
+              releaseDate: '1991, August 12',
+              genres: [ 'heavy metal' ]
+            });
+albumList.push({
+              artistName: 'The Prodigy',
+              name: 'Music for the Jilted Generation',
+              releaseDate: '1994, July 4',
+              genres: [ 'electronica', 'breakbeat hardcore', 'rave', 'jungle' ]
+            });
+albumList.push({
+              artistName: 'Johnny Cash',
+              name: 'Unchained',
+              releaseDate: '1996, November 5',
+              genres: [ 'country', 'rock' ]
+            });
+
 
 db.Album.remove({}, function(err, albums){
 
-  db.Album.create(albumsList, function(err, albums){
+  db.Album.create(albumList, function(err, albums){
     if (err) { return console.log('ERROR', err); }
     console.log("all albums:", albums);
     console.log("created", albums.length, "albums");
