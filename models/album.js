554a7168f129010597cda55eff809be9ef33d6d0
tgaff <tgaff@alumni.nd.edu>
2015-12-07 21:21:37 -0800

    Sprint 3 Step 1

--- models/album.js
+++ models/album.js
@@ -1,11 +1,14 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
+var Song = require('./song');
+
 var AlbumSchema = new Schema({
   artistName: String,
   name: String,
   releaseDate: String,
-  genres: [ String ]
+  genres: [ String ],
+  songs: [Song.schema]
 });
 
 var Album = mongoose.model('Album', AlbumSchema);
