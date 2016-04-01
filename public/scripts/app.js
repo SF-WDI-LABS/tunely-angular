340e1630ed927780100c91fdde29baf591819a27
tgaff <tgaff@alumni.nd.edu>
2016-04-01 09:19:51 -0700

    sprint 3 step 5 complete - modal works or would if we had a server route setup

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -33,8 +33,10 @@ $(document).ready(function() {
       console.log('id',currentAlbumId);
       $('#songModal').data('album-id', currentAlbumId);
       $('#songModal').modal();  // display the modal!
-
   });
+
+  // save song modal save button
+  $('#saveSong').on('click', handleNewSongSubmit);
 
 });
 
@@ -47,4 +49,38 @@ function renderAlbum(album) {
   var albumsTemplate = Handlebars.compile(albumHtml);
   var html = albumsTemplate(album);
   $('#albums').prepend(html);
+}
+
+function handleNewSongSubmit(e) {
+  e.preventDefault();
+  var $modal = $('#songModal');
+  var $songNameField = $modal.find('#songName');
+  var $trackNumberField = $modal.find('#trackNumber');
+
+  // get data from modal fields
+  // note the server expects the keys to be 'name', 'trackNumber' so we use those.
+  var dataToPost = {
+    name: $songNameField.val(),
+    trackNumber: $trackNumberField.val()
+  };
+  var albumId = $modal.data('albumId');
+  console.log('retrieved songName:', songName, ' and trackNumber:', trackNumber, ' for album w/ id: ', albumId);
+  // POST to SERVER
+  var songPostToServerUrl = '/api/albums/'+ albumId + '/songs';
+  $.post(songPostToServerUrl, dataToPost, function(data) {
+    console.log('received data from post to /songs:', data);
+    // clear form
+    $songNameField.val('');
+    $trackNumberField.val('');
+
+    // close modal
+    $modal.modal('hide');
+    // update the correct album to show the new song
+    // Note there are a couple of ways we could do this.
+    // 1. re-retrieve the entire album and call renderAlbum with it (cost: extra server round-trip)
+    // 2. allow the server to respond with the entire album and then renderAlbum (slightly less standard)
+    console.log('for now lets log the results and wait till we have the server setup:', data);
+  }).error(function(err) {
+    console.log('post to /api/albums/:albumId/songs resulted in error', err);
+  });
 }
