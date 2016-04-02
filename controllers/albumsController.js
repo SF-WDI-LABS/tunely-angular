abe656acb21f1af616f83ef91f4f407e43c8ba42
tgaff <tgaff@alumni.nd.edu>
2016-04-02 15:56:35 -0700

    Provide empty template controller for estudiantes

--- controllers/albumsController.js
+++ controllers/albumsController.js
@@ -39,7 +39,28 @@ function index(req, res) {
   // FILL ME IN !
 }
 
+function create(req, res) {
+  // FILL ME IN !
+}
+
+function show(req, res) {
+  // FILL ME IN !
+}
+
+function destroy(req, res) {
+  // FILL ME IN !
+}
+
+function update(req, res) {
+  // FILL ME IN !
+}
+
+
 // export public methods here
 module.exports = {
-  index: index
+  index: index,
+  create: create,
+  show: show,
+  destroy: destroy,
+  update: update
 };
