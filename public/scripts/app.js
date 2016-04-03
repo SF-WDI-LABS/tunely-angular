6765a0c3d282bbc0ec41cd02d21012bcb3e09cd5
tgaff <tgaff@alumni.nd.edu>
2016-04-02 17:43:03 -0700

    Sprint 4 step 3 - delete works

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -35,10 +35,22 @@ $(document).ready(function() {
 
 });
 
-
+// when a delete button for an album is clicked
 function handleDeleteAlbumClick(e) {
   var albumId = $(this).parents('.album').data('album-id');
   console.log('someone wants to delete album id=' + albumId );
+  $.ajax({
+    url: '/api/albums/' + albumId,
+    method: 'DELETE',
+    success: handleDeleteAlbumSuccess
+  });
+}
+
+// callback after DELETE /api/albums/:id
+function handleDeleteAlbumSuccess(data) {
+  var deletedAlbumId = data._id;
+  console.log('removing the following album from the page:', deletedAlbumId);
+  $('div[data-album-id=' + deletedAlbumId + ']').remove();
 }
 
 // this function takes a single album and renders it to the page
