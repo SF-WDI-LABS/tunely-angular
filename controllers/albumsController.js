f388d671d79f266a92bdc8c8bcaf255c38bad45f
Juliana <jlopker@gmail.com>
2016-04-05 11:21:50 -0700

    change albums controller to not require genres

--- controllers/albumsController.js
+++ controllers/albumsController.js
@@ -17,8 +17,10 @@ function create(req, res) {
   console.log('body', req.body);
 
   // split at comma and remove and trailing space
-  var genres = req.body.genres.split(',').map(function(item) { return item.trim(); } );
-  req.body.genres = genres;
+  if (req.body.genres) {
+    var genres = req.body.genres.split(',').map(function(item) { return item.trim(); } );
+    req.body.genres = genres;
+  }
 
   db.Album.create(req.body, function(err, album) {
     if (err) { console.log('error', err); }
