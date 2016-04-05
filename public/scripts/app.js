7a417b238e5e777887d00103e9222145c66110c0
Juliana <jlopker@gmail.com>
2016-04-04 22:37:51 -0700

    empty front end

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,35 +1,5 @@
 /* CLIENT-SIDE JS
  *
- * You may edit this file as you see fit.  Try to separate different components
- * into functions and objects as needed.
+ * This is your main angular file. Edit as you see fit.
  *
  */
-
-angular
-  .module('tunely', [])
-  .controller('AlbumsIndexController', AlbumsIndexController);
-
-function AlbumsIndexController () {
-  var vm = this;
-  vm.newAlbum = {};
-
-  vm.newAlbum = {
-      name: 'Viva Hate',
-      artistName: 'Morrissey'
-  };
-
-  vm.albums = [
-    {
-      name: 'Coming Home',
-      artistName: 'Leon Bridges'
-    },
-    {
-      name: 'Are We There',
-      artistName: 'Sharon Van Etten'
-    },
-    {
-      name: 'The Queen is Dead',
-      artistName: 'The Smiths'
-    }
-  ];
-}
