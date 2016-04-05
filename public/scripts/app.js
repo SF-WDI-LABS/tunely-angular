ab4651ab3f052f84bd78e24889ffea24a7684b34
Juliana <jlopker@gmail.com>
2016-04-04 21:51:34 -0700

    sprint 1 draft

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,4 +5,31 @@
  *
  */
 
-angular.module('tunely');
+angular
+  .module('tunely', [])
+  .controller('AlbumsIndexController', AlbumsIndexController);
+
+function AlbumsIndexController () {
+  var vm = this;
+  vm.newAlbum = {};
+
+  vm.newAlbum = {
+      name: 'Viva Hate',
+      artistName: 'Morrissey'
+  };
+
+  vm.albums = [
+    {
+      name: 'Coming Home',
+      artistName: 'Leon Bridges'
+    },
+    {
+      name: 'Are We There',
+      artistName: 'Sharon Van Etten'
+    },
+    {
+      name: 'The Queen is Dead',
+      artistName: 'The Smiths'
+    }
+  ];
+}
