4f1498dcf91fa2894b030ef4ef4cd0bcc702cb8f
Juliana <jlopker@gmail.com>
2016-04-05 12:46:19 -0700

    added update and delete functions

--- public/scripts/controllers/AlbumsIndexController.js
+++ public/scripts/controllers/AlbumsIndexController.js
@@ -32,4 +32,36 @@ function AlbumsIndexController ($http) {
       console.log('There was an error posting the data', response);
     });
   }
+
+  vm.editAlbum = function (album) {
+    $http({
+      method: 'PUT',
+      url: '/api/albums/'+album._id,
+      data: album
+    }).then(function successCallback(json) {
+      // don't need to do anything!
+    }, function errorCallback(response) {
+      console.log('There was an error editing the data', response);
+    });
+  }
+
+  vm.deleteAlbum = function (id) {
+    $http({
+      method: 'DELETE',
+      url: '/api/albums/'+ id
+    }).then(function successCallback(json) {
+      var index = findWithAttr(vm.albums, '_id', id);
+      vm.albums.splice(index, 1);
+    }, function errorCallback(response) {
+      console.log('There was an error deleting the data', response);
+    });
+  }
+
+  function findWithAttr(array, attr, value) {
+    for(var i = 0; i < array.length; i += 1) {
+      if(array[i][attr] === value) {
+          return i;
+      }
+    }
+  }
 }
