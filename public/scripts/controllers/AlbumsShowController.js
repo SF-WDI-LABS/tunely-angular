73d418af943cc4cbbb4fc1e88b5c4ed246a9fbcc
Juliana <jlopker@gmail.com>
2016-04-13 14:28:09 -0700

    added error callback

--- public/scripts/controllers/AlbumsShowController.js
+++ public/scripts/controllers/AlbumsShowController.js
@@ -13,6 +13,8 @@ function AlbumsShowController ($http, $routeParams) {
     url: '/api/albums/'+$routeParams.id
   }).then(function successCallback(json) {
     vm.album = json.data;
+  }, function errorCallback(response) {
+    console.log('There was an error getting the data', response);
   });
 
 }
