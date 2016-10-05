6b6c9efde6e0387754cc4055300bd413b53e95d7
lordshadowist <shadowist@live.com>
2016-10-04 21:29:42 -0700

    Finish Sprint 1

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,3 +3,34 @@
  * This is your main angular file. Edit as you see fit.
  *
  */
+
+ 
+
+ angular
+  .module('tunely', [])
+  .controller('AlbumsIndexController', AlbumsIndexController);
+
+function AlbumsIndexController () {
+  var vm = this;
+  vm.newAlbum = {};
+
+  vm.newAlbum = {
+      name: 'License to Ill',
+      artistName: 'Beastie Boys'
+  };
+
+  vm.albums = [
+  {
+    name: 'Coming Home',
+    artistName: 'Leon Bridges'
+  },
+  {
+    name: 'Are We There',
+    artistName: 'Sharon Van Etten'
+  },
+  {
+    name: 'The Queen is Dead',
+    artistName: 'The Smiths'
+  }
+];
+}
