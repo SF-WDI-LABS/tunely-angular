10794ee61aebf3869529b120b952b92f8cb87e00
lordshadowist <shadowist@live.com>
2016-10-05 22:02:35 -0700

    Finish Sprint 2

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,28 +10,38 @@ angular
   // ^ the first argument is a string naming the controller,
   // the second argument is a function that defines the capacities
   // of the controller.
-
-function AlbumsIndexController () {
+AlbumsIndexController.$inject = ['$http'];
+function AlbumsIndexController ($http) {
   var vm = this;
+  vm.albums = [];
   vm.newAlbum = {};
 
   vm.newAlbum = {
       name: 'Viva Hate',
-      artistName: 'Morrissey'
+      artistName: 'Morrissey',
+      genresAsString: "",
+      genres: []
   };
 
-  vm.albums = [
-    {
-      name: 'Coming Home',
-      artistName: 'Leon Bridges'
-    },
-    {
-      name: 'Are We There',
-      artistName: 'Sharon Van Etten'
-    },
-    {
-      name: 'The Queen is Dead',
-      artistName: 'The Smiths'
-    }
-  ];
+  $http(
+  {
+    method: 'GET',
+    url: '/api/albums',
+  }).then(function(response)
+  {
+    vm.albums = response.data;
+  });
+
+  vm.createAlbum = function () {
+    vm.newAlbum.genres = vm.newAlbum.genresAsString.split(',')
+  $http({
+    method: 'POST',
+    url: '/api/albums',
+    data: vm.newAlbum
+  }).then(function successCallback(response) {
+    vm.albums.push(response.data);
+  }, function errorCallback(response) {
+    console.log('There was an error posting the data', response);
+  });
+}
 }
